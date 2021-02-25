a5970df34f88774c069d3320ea52a317a107be79
stasatdaglabs <stas@daglabs.com>
2021-02-25 15:44:08 +0200

    Split the componentManager to its respective parts.

--- node/node.go
+++ node/node.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"github.com/kaspanet/kaspad/app"
+	"github.com/kaspanet/kaspad/app/protocol"
+	domainPackage "github.com/kaspanet/kaspad/domain"
 	kaspadConfigPackage "github.com/kaspanet/kaspad/infrastructure/config"
+	"github.com/kaspanet/kaspad/infrastructure/network/addressmanager"
+	"github.com/kaspanet/kaspad/infrastructure/network/connmanager"
+	"github.com/kaspanet/kaspad/infrastructure/network/netadapter"
+	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
 	configPackage "github.com/stasatdaglabs/kaspa-dag-visualizer/node/infrastructure/config"
 	"github.com/stasatdaglabs/kaspa-dag-visualizer/node/infrastructure/database"
 	"github.com/stasatdaglabs/kaspa-dag-visualizer/node/infrastructure/logging"
@@ -17,6 +22,26 @@ func main() {
 		logErrorAndExit("Could not parse command line arguments.\n%s", err)
 	}
 
+	node, err := newNode(config)
+	if err != nil {
+		logErrorAndExit("Could not create node: %s", err)
+	}
+	err = node.start()
+	if err != nil {
+		logErrorAndExit("Could not start node: %s", err)
+	}
+
+	log.Infof("Node started!")
+	<-make(chan struct{})
+}
+
+type node struct {
+	netAdapter        *netadapter.NetAdapter
+	connectionManager *connmanager.ConnectionManager
+	protocolManager   *protocol.Manager
+}
+
+func newNode(config *configPackage.Config) (*node, error) {
 	kaspadConfig := &kaspadConfigPackage.Config{
 		Flags: &kaspadConfigPackage.Flags{
 			ConnectPeers: []string{config.P2PServerAddress},
@@ -25,19 +50,43 @@ func main() {
 	}
 	databaseContext, err := database.Open()
 	if err != nil {
-		logErrorAndExit("Could not open database: %s", err)
+		return nil, err
 	}
-	interrupt := make(chan struct{})
-
-	componentManager, err := app.NewComponentManager(kaspadConfig, databaseContext, interrupt)
+	domain, err := domainPackage.New(config.NetworkFlags.ActiveNetParams, databaseContext, false)
 	if err != nil {
-		logErrorAndExit("Could not start kaspad: %s", err)
+		return nil, err
 	}
-	componentManager.Start()
+	netAdapter, err := netadapter.NewNetAdapter(kaspadConfig)
+	if err != nil {
+		return nil, err
+	}
+	netAdapter.SetRPCRouterInitializer(func(router *router.Router, connection *netadapter.NetConnection) {})
+	addressManager, err := addressmanager.New(addressmanager.NewConfig(kaspadConfig), databaseContext)
+	if err != nil {
+		return nil, err
+	}
+	connectionManager, err := connmanager.New(kaspadConfig, netAdapter, addressManager)
+	if err != nil {
+		return nil, err
+	}
+	protocolManager, err := protocol.NewManager(kaspadConfig, domain, netAdapter, addressManager, connectionManager)
+	if err != nil {
+		return nil, err
+	}
+	return &node{
+		netAdapter:        netAdapter,
+		connectionManager: connectionManager,
+		protocolManager:   protocolManager,
+	}, nil
+}
 
-	log.Infof("Node started!")
-
-	<-interrupt
+func (n *node) start() error {
+	err := n.netAdapter.Start()
+	if err != nil {
+		return err
+	}
+	n.connectionManager.Start()
+	return nil
 }
 
 func logErrorAndExit(errorLog string, logParameters ...interface{}) {
