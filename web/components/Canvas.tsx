f09d809a40cfd2e6c58d7a56a93b3719902ef4f0
stasatdaglabs <stas@daglabs.com>
2021-03-01 13:48:50 +0200

    Stretch a canvas across the entire screen.

---
+++ web/components/Canvas.tsx
@@ -0,0 +1,18 @@
+import {useEffect, useRef} from 'react'
+import style from '../styles/Canvas.module.css'
+
+const Canvas = props => {
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+
+        context.fillStyle = '#ff0000'
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+    }, [])
+
+    return <canvas ref={canvasRef} className={style.canvas} {...props}/>
+}
+
+export default Canvas
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Canvas from '../components/Canvas'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>kaspa-dag-visualizer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>kaspa-dag-visualizer</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Canvas/>
+        </div>
+    )
 }
