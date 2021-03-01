1ff0eb16e5fa280113ccbb3b187564aab7338e4e
stasatdaglabs <stas@daglabs.com>
2021-03-01 14:52:49 +0200

    Add favicons.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,7 +7,9 @@ export default function Home() {
         <div className={styles.container}>
             <Head>
                 <title>kaspa-dag-visualizer</title>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" type="image/png" href="./favicon.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+                <link rel="mask-icon" href="./kaspa_mask_icon.svg" color="#F05A28" />
             </Head>
             <Canvas/>
             <div className={styles.header}>
