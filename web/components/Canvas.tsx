6fca8ca0e2cbc9066bcca1824ecc37f0495004c7
stasatdaglabs <stas@daglabs.com>
2021-03-01 14:37:16 +0200

    Implement a render loop.

--- web/components/Canvas.tsx
+++ web/components/Canvas.tsx
@@ -2,29 +2,46 @@ import {useEffect, useRef} from 'react'
 import style from '../styles/Canvas.module.css'
 
 const Canvas = props => {
-    const canvasRef = useRef(null)
+    const canvasRef = useRef(null);
 
     useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-
-        resizeIfRequired(canvas)
-
-        context.fillStyle = '#ffffff'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-
-        context.fillStyle = '#ff0000'
-        context.fillRect(200, 100, 100, 100)
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        const renderer = new Renderer(canvas, context);
+        renderer.render();
     }, [])
 
     return <canvas ref={canvasRef} className={style.canvas} {...props}/>
 }
 
-const resizeIfRequired = (canvas: HTMLCanvasElement) => {
-    const {width, height} = canvas.getBoundingClientRect()
-    if (canvas.width !== width || canvas.height !== height) {
-        canvas.width = width
-        canvas.height = height
+class Renderer {
+    private readonly canvas: HTMLCanvasElement;
+    private readonly context: CanvasRenderingContext2D;
+
+    constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+        this.canvas = canvas;
+        this.context = context;
+        this.render = this.render.bind(this);
+    }
+
+    render() {
+        this.resizeIfRequired(this.canvas);
+
+        this.context.fillStyle = '#ffffff';
+        this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+
+        this.context.fillStyle = '#ff0000';
+        this.context.fillRect(200, 100, 100, 100);
+
+        requestAnimationFrame(this.render);
+    }
+
+    resizeIfRequired(canvas: HTMLCanvasElement) {
+        const {width, height} = canvas.getBoundingClientRect()
+        if (canvas.width !== width || canvas.height !== height) {
+            canvas.width = width
+            canvas.height = height
+        }
     }
 }
 
