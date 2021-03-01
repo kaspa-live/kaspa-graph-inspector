4af4251a72717da68596ce208df1f4c04491b352
stasatdaglabs <stas@daglabs.com>
2021-03-01 14:18:54 +0200

    Add a resize function.

--- web/components/Canvas.tsx
+++ web/components/Canvas.tsx
@@ -8,14 +8,24 @@ const Canvas = props => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
 
+        resizeIfRequired(canvas)
+
         context.fillStyle = '#ffffff'
         context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
         context.fillStyle = '#ff0000'
-        context.fillRect(100, 100, 100, 100)
+        context.fillRect(200, 100, 100, 100)
     }, [])
 
     return <canvas ref={canvasRef} className={style.canvas} {...props}/>
+}
+
+const resizeIfRequired = (canvas: HTMLCanvasElement) => {
+    const {width, height} = canvas.getBoundingClientRect()
+    if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width
+        canvas.height = height
+    }
 }
 
 export default Canvas
