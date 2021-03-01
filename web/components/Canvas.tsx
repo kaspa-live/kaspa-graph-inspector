5b1a20266b7c5b803f3fa5873e6520baafe2404d
stasatdaglabs <stas@daglabs.com>
2021-03-01 16:09:56 +0200

    Move a circle on the canvas.

--- web/components/Canvas.tsx
+++ web/components/Canvas.tsx
@@ -17,7 +17,9 @@ const Canvas = props => {
 
 class PixiRenderer {
     private pixi;
-    private pixiApplication;
+    private pixiApplication: PIXI.Application;
+
+    private circle: PIXI.Graphics;
 
     constructor(canvas: HTMLCanvasElement) {
         import("pixi.js").then(pixi => {
@@ -27,8 +29,24 @@ class PixiRenderer {
                 view: canvas,
                 resizeTo: canvas,
             });
+            this.initialize();
+            this.update = this.update.bind(this);
+            this.pixiApplication.ticker.add(this.update);
             this.pixiApplication.start();
         });
+    }
+
+    initialize() {
+        this.circle = new this.pixi.Graphics();
+        this.circle.beginFill(0xff0000);
+        this.circle.drawCircle(30, 30, 30);
+        this.circle.endFill();
+        this.pixiApplication.stage.addChild(this.circle);
+    }
+
+    update(deltaTime: number) {
+        this.circle.x += 0.1 * deltaTime;
+        this.circle.y += 0.1 * deltaTime;
     }
 
     stop() {
