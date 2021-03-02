8cc5589677562434f1f086894b036f7c0254c9c0
stasatdaglabs <stas@daglabs.com>
2021-03-02 13:11:12 +0200

    Setup express boilerplate.

--- api/src/api.ts
+++ api/src/api.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+import express from 'express'
+
+const server = express();
+server.get('/', (request, response) => {
+    response.send("Hello world!");
+});
+
+const port = 3001;
+server.listen(port, () => {
+    console.log(`API server listening on port ${port}...`);
+});
