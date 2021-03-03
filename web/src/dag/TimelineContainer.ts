0c3eb7dd61287e08d6a027581369e40e582dd6d3
stasatdaglabs <stas@daglabs.com>
2021-03-03 11:00:05 +0200

    Group the blocks by height.

--- web/src/dag/TimelineContainer.ts
+++ web/src/dag/TimelineContainer.ts
@@ -5,6 +5,7 @@ import BlockSprite from "./BlockSprite";
 export default class TimelineContainer extends PIXI.Container {
     private readonly application: PIXI.Application;
     private readonly blockIdsToBlockSprites: { [id: number]: BlockSprite } = {};
+    private readonly heightGroups: { [height: number]: number[] } = {};
 
     constructor(application: PIXI.Application) {
         super();
@@ -14,16 +15,35 @@ export default class TimelineContainer extends PIXI.Container {
         fetch("http://localhost:3001/blocks?startHeight=0&endHeight=100")
             .then(response => response.json())
             .then((blocks: Block[]) => {
+                let shouldRecalculateBlockSpritePositions = false;
                 for (let block of blocks) {
                     if (!this.blockIdsToBlockSprites[block.id]) {
-                        this.blockIdsToBlockSprites[block.id] = new BlockSprite(this.application, block);
+                        // Add the block to its "height group"--an ordered set of
+                        // blocks with the same height
+                        if (!this.heightGroups[block.height]) {
+                            this.heightGroups[block.height] = [];
+                        }
+                        this.heightGroups[block.height].push(block.id);
+
+                        // Add the block to the block-by-ID map
+                        const blockSprite = new BlockSprite(this.application, block);
+                        this.blockIdsToBlockSprites[block.id] = blockSprite;
+
+                        // Add the block sprite to the timeline container
+                        this.addChild(blockSprite);
+
+                        // The timeline container changed so the block
+                        // sprite positions needs to be recalculated
+                        shouldRecalculateBlockSpritePositions = true;
                     }
                 }
-                this.redraw();
+                if (shouldRecalculateBlockSpritePositions) {
+                    this.recalculateBlockSpritePositions();
+                }
             });
     }
 
-    private redraw() {
-        this.removeChildren();
+    private recalculateBlockSpritePositions() {
+
     }
 }
