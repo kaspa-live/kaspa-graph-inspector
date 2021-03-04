19ac6b36e6c0a906c9b78337b09fd9f0c8467de7
stasatdaglabs <stas@daglabs.com>
2021-03-04 12:37:04 +0200

    Implement refetching data every second.

--- web/src/dag/Dag.ts
+++ web/src/dag/Dag.ts
@@ -2,11 +2,19 @@ import * as PIXI from 'pixi.js'
 import TimelineContainer from "./TimelineContainer";
 
 export default class Dag {
+    private readonly tickInternalInMilliseconds = 1000;
+
     private readonly application: PIXI.Application;
     private readonly timelineContainer: TimelineContainer;
 
     private currentWidth: number = 0;
     private currentHeight: number = 0;
+
+    private targetHeight: number | null = null;
+
+    private currentTickFunction: () => Promise<void> = async () => {
+        // Do nothing
+    };
 
     constructor(canvas: HTMLCanvasElement) {
         this.application = new PIXI.Application({
@@ -19,10 +27,11 @@ export default class Dag {
         this.timelineContainer = new TimelineContainer(this.application);
         this.application.ticker.add(this.resizeIfRequired);
         this.application.stage.addChild(this.timelineContainer);
-        this.initializeTargetHeightByQueryParams();
-
 
         this.application.start();
+
+        this.resolveInitialTickFunction();
+        this.start();
     }
 
     private resizeIfRequired = () => {
@@ -35,34 +44,38 @@ export default class Dag {
         }
     }
 
-    private initializeTargetHeightByQueryParams = () => {
-        const targetHeight = this.extractTargetHeightFromParams();
-        if (targetHeight !== null) {
-            this.setTargetHeight(targetHeight);
-            return;
-        }
-        this.setTargetHeight(0);
-    }
-
-    private extractTargetHeightFromParams = (): number | null => {
+    private resolveInitialTickFunction = () => {
         const urlParams = new URLSearchParams(window.location.search);
         const targetHeightString = urlParams.get("targetHeight");
         if (targetHeightString) {
             const targetHeight = parseInt(targetHeightString);
             if (targetHeight) {
-                return targetHeight;
+                this.targetHeight = targetHeight;
+                this.currentTickFunction = this.trackTargetHeight;
             }
         }
-        return null;
     }
 
-    private setTargetHeight(targetHeight: number) {
+    private start = () => {
+        this.tick();
+    }
+
+    private tick = () => {
+        this.currentTickFunction().then(this.scheduleNextTick);
+    }
+
+    private scheduleNextTick = () => {
+        window.setTimeout(this.tick, this.tickInternalInMilliseconds);
+    }
+
+    private trackTargetHeight = async () => {
+        const targetHeight = this.targetHeight as number;
         this.timelineContainer.setTargetHeight(targetHeight);
 
         const [startHeight, endHeight] = this.timelineContainer.getVisibleHeightRange(targetHeight);
-        fetch(`http://localhost:3001/blocks?startHeight=${startHeight}&endHeight=${endHeight}`)
-            .then(response => response.json())
-            .then(this.timelineContainer.insertOrIgnoreBlocks);
+        const response = await fetch(`http://localhost:3001/blocks?startHeight=${startHeight}&endHeight=${endHeight}`);
+        const blocks = await response.json();
+        this.timelineContainer.insertOrIgnoreBlocks(blocks);
     }
 
     stop = () => {
