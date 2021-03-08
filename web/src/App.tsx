38b24955e0ad247d39716b968acb2150cc7de3e4
stasatdaglabs <stas@daglabs.com>
2021-03-08 12:33:01 +0200

    Replace the old header with a nice MUI one.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,32 @@
 import './App.css';
 import Canvas from "./Canvas";
+import {AppBar, makeStyles, Toolbar, Typography} from "@material-ui/core";
+import Logo from "./Logo";
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div className="container">
+            <AppBar position="static">
+                <Toolbar>
+                    <Logo/>
+                    <Typography variant="h6" className={classes.title}>
+                        Kaspa Grapher
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <Canvas/>
-            <div className="header">
-                <div className="logo"/>
-                <div className="title">kaspa-dag-visualizer</div>
-            </div>
         </div>
     );
 }
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        flexGrow: 1,
+        marginTop: 5,
+        marginLeft: 10,
+    },
+}));
 
 export default App;
---
+++ web/src/Logo.tsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+    return <div className="logo"/>
+};
+
+export default Logo;
