07ce9262b6acae23e7199ebbcf1267afbb563f62
stasatdaglabs <stas@daglabs.com>
2021-03-10 15:14:11 +0200

    Modify the app theme a bit.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,17 +2,31 @@ import './App.css';
 import Canvas from "./Canvas";
 import {useState} from "react";
 import Header from "./Header";
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
 
 const App = () => {
     const [isTrackingState, setTrackingState] = useState(true);
     const [dag, setDag] = useState();
 
     return (
-        <div className="container">
-            <Header isTrackingState={isTrackingState} dag={dag}/>
-            <Canvas setTrackingState={setTrackingState} setDag={setDag}/>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className="container">
+                <Header isTrackingState={isTrackingState} dag={dag}/>
+                <Canvas setTrackingState={setTrackingState} setDag={setDag}/>
+            </div>
+        </ThemeProvider>
     );
 }
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#175676"
+        },
+        secondary: {
+            main: "#d62839"
+        }
+    }
+});
 
 export default App;
