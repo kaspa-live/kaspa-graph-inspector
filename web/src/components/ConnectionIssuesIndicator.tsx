d3036b52400514ffd140fbd21db50f32b05dfb71
stasatdaglabs <stas@daglabs.com>
2021-03-21 15:07:00 +0200

    Add tool tips to the track button and the connection issues indicator.

--- web/src/components/ConnectionIssuesIndicator.tsx
+++ web/src/components/ConnectionIssuesIndicator.tsx
@@ -1,14 +1,18 @@
-import {Box, CircularProgress, Typography} from "@material-ui/core";
+import {Box, CircularProgress, IconButton, Tooltip, Typography} from "@material-ui/core";
 
 const ConnectionIssuesIndicator = () => {
     return (
-        <Box position="relative" display="inline-flex">
-            <CircularProgress color="inherit" thickness={6.0} size="24px"/>
-            <Box top={3} left={0} bottom={0} right={0} position="absolute" display="flex"
-                 alignItems="center" justifyContent="center" fontWeight="fontWeightBold">
-                <Typography variant="button" component="div" color="inherit">!</Typography>
-            </Box>
-        </Box>
+        <Tooltip title="Connection is unstable">
+            <IconButton color="inherit">
+                <Box position="relative" display="inline-flex">
+                    <CircularProgress color="inherit" thickness={6.0} size="24px"/>
+                    <Box top={3} left={0} bottom={0} right={0} position="absolute" display="flex"
+                         alignItems="center" justifyContent="center" fontWeight="fontWeightBold">
+                        <Typography variant="button" component="div" color="inherit">!</Typography>
+                    </Box>
+                </Box>
+            </IconButton>
+        </Tooltip>
     );
 }
 
--- web/src/components/TrackButton.tsx
+++ web/src/components/TrackButton.tsx
@@ -1,11 +1,13 @@
 import FastForwardIcon from "@material-ui/icons/FastForward";
-import {IconButton} from "@material-ui/core";
+import {IconButton, Tooltip} from "@material-ui/core";
 
 const TrackButton = ({onClick}: { onClick: () => void }) => {
     return (
-        <IconButton color="inherit" onClick={onClick}>
-            <FastForwardIcon/>
-        </IconButton>
+        <Tooltip title="Track DAG tips">
+            <IconButton color="inherit" onClick={onClick}>
+                <FastForwardIcon/>
+            </IconButton>
+        </Tooltip>
     );
 }
 
