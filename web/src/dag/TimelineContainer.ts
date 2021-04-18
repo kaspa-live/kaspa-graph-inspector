27d467992e427abb6f0a88c32b18ee48b7b69fa8
stasatdaglabs <stas@daglabs.com>
2021-04-18 15:31:59 +0300

    Resolve block Y positions using serverside height indexes.

--- web/src/dag/TimelineContainer.ts
+++ web/src/dag/TimelineContainer.ts
@@ -15,7 +15,6 @@ export default class TimelineContainer extends PIXI.Container {
     private readonly edgeContainer: PIXI.Container;
     private readonly blockContainer: PIXI.Container;
 
-    private readonly heightGroups: { [height: number]: number[] } = {};
     private readonly heightsToHeightSprites: { [height: number]: HeightSprite } = {};
     private readonly blockIdsToBlockSprites: { [id: number]: BlockSprite } = {};
     private readonly blockIdsToEdgeSprites: { [id: number]: EdgeSprite[] } = {};
@@ -55,16 +54,11 @@ export default class TimelineContainer extends PIXI.Container {
             this.blockIdsToBlocks[block.id] = block;
         }
 
-        // Remove no-longer relevant height groups
+        // Remove no-longer relevant height sprites
         const heightsInBlocks: { [height: number]: boolean } = {};
         for (let block of blocks) {
             heightsInBlocks[block.height] = true;
         }
-        Object.keys(this.heightGroups)
-            .filter(height => !heightsInBlocks[parseInt(height)])
-            .forEach(height => delete this.heightGroups[parseInt(height)]);
-
-        // Remove no-longer relevant height sprites
         Object.entries(this.heightsToHeightSprites)
             .filter(([height, _]) => !heightsInBlocks[parseInt(height)])
             .forEach(([height, sprite]) => {
@@ -96,23 +90,6 @@ export default class TimelineContainer extends PIXI.Container {
             if (this.blockIdsToBlockSprites[block.id]) {
                 const blockSprite = this.blockIdsToBlockSprites[block.id];
                 blockSprite.setColor(block.color);
-            }
-        }
-
-        // Add new blocks to their appropriate height groups
-        for (let block of blocks) {
-            if (!this.heightGroups[block.height]) {
-                this.heightGroups[block.height] = [];
-            }
-            let exists = false;
-            for (let blockId of this.heightGroups[block.height]) {
-                if (blockId === block.id) {
-                    exists = true;
-                    break;
-                }
-            }
-            if (!exists) {
-                this.heightGroups[block.height].push(block.id);
             }
         }
 
@@ -212,16 +189,22 @@ export default class TimelineContainer extends PIXI.Container {
         const blockSize = this.calculateBlockSize(rendererHeight);
         const margin = this.calculateMargin(blockSize);
 
-        Object.values(this.heightGroups).forEach(blockIds => {
-            for (let i = 0; i < blockIds.length; i++) {
-                const blockId = blockIds[i];
-                const blockSprite = this.blockIdsToBlockSprites[blockId];
+        const heightGroupSizes: {[height: number]: number} = {};
+        Object.values(this.blockIdsToBlocks).forEach(block => {
+            if (!heightGroupSizes[block.height]) {
+                heightGroupSizes[block.height] = 0;
+            }
+            heightGroupSizes[block.height]++;
+        });
+
+        Object.entries(this.blockIdsToBlocks)
+            .forEach(([blockId, block]) => {
+                const blockSprite = this.blockIdsToBlockSprites[parseInt(blockId)];
                 blockSprite.setSize(blockSize);
 
-                const block = this.blockIdsToBlocks[blockId];
+                const heightGroupSize = heightGroupSizes[block.height];
                 blockSprite.x = this.calculateBlockSpriteX(block.height, blockSize, margin);
-                blockSprite.y = this.calculateBlockSpriteY(i, blockIds.length, rendererHeight);
-            }
+                blockSprite.y = this.calculateBlockSpriteY(block.heightGroupIndex, heightGroupSize, rendererHeight);
         });
     }
 
