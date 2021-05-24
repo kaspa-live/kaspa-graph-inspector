f92da3345f772af7ed889fe043ca44cd33070b05
stasatdaglabs <stas@daglabs.com>
2021-05-24 15:59:50 +0300

    Add a drop shadow to the block information panel.

--- web/src/components/BlockInformationPanel.tsx
+++ web/src/components/BlockInformationPanel.tsx
@@ -1,12 +1,17 @@
 import {Block} from "../dag/model/Block";
 import './BlockInformationPanel.css'
+import {Paper} from "@material-ui/core";
 
 const BlockInformationPanel = ({block}: { block: Block | null }) => {
     if (!block) {
         return <div/>;
     }
 
-    return <div className="block-information-panel">{block.blockHash}</div>
+    return <Paper elevation={4}>
+        <div className="block-information-panel">
+            {block.blockHash}
+        </div>
+    </Paper>
 }
 
 export default BlockInformationPanel;
