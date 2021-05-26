8a378d8f33b5c0595982e496d3602f3025ee87ba
stasatdaglabs <stas@daglabs.com>
2021-05-26 14:43:27 +0300

    Add tooltips to hashes in the block information panel.

--- web/src/components/BlockInformationPanelHash.tsx
+++ web/src/components/BlockInformationPanelHash.tsx
@@ -1,15 +1,17 @@
 import './BlockInformationPanelHash.css'
-import {Typography} from "@material-ui/core";
+import {Tooltip, Typography} from "@material-ui/core";
 
 const BlockInformationPanelHash = ({hash}: { hash: string }) => {
     const hashStart = hash.substring(0, 16);
     const hashEnd = hash.substring(hash.length - 8);
 
-    return <Typography className="block-information-panel-hash" variant="h6">
-        <div className="hash-start">{hashStart}</div>
-        <div className="hash-ellipsis">…</div>
-        <div className="hash-end">{hashEnd}</div>
-    </Typography>;
+    return <Tooltip title={hash} arrow interactive>
+        <Typography className="block-information-panel-hash" variant="h6">
+            <div className="hash-start">{hashStart}</div>
+            <div className="hash-ellipsis">…</div>
+            <div className="hash-end">{hashEnd}</div>
+        </Typography>
+    </Tooltip>;
 };
 
 export default BlockInformationPanelHash;
