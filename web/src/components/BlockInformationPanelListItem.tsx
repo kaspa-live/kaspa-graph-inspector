7c0ec5781a6ca8e900db8325538ce4f911e0de3f
stasatdaglabs <stas@daglabs.com>
2021-05-26 12:18:13 +0300

    Add an information icon to block information panel list items.

--- web/src/components/BlockInformationPanelListItem.tsx
+++ web/src/components/BlockInformationPanelListItem.tsx
@@ -1,12 +1,16 @@
 import {ListItem, Typography} from "@material-ui/core";
 import './BlockInformationPanelListItem.css'
 import {ReactNode} from "react";
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 const BlockInformationPanelListItem = ({children, label}: { children: ReactNode, label: string }) => {
     return <ListItem className="block-information-panel-list-item" disableGutters>
-        <Typography className="label" variant="h6">
-            {label}
-        </Typography>
+        <div className="header">
+            <Typography className="label" variant="h6">
+                {label}
+            </Typography>
+            <InfoOutlinedIcon className="info-icon"/>
+        </div>
         {children}
     </ListItem>
 };
