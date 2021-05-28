8a94a8347aa36a556cd0eee1c1e1313ecabeaf28
stasatdaglabs <stas@daglabs.com>
2021-05-28 15:59:18 +0300

    Make the block information contents scrollable.

--- web/src/components/BlockInformationPanel.tsx
+++ web/src/components/BlockInformationPanel.tsx
@@ -59,53 +59,57 @@ const BlockInformationPanel = ({blockInformation, onClose}:
                     <CloseIcon/>
                 </IconButton>
             </div>
-            <div className="block-information-content">
-                <List>
-                    <BlockInformationPanelListItem label="Block Hash" tooltip={tooltip}>
-                        <BlockInformationPanelHash hash={blockInformation.block.blockHash}/>
-                    </BlockInformationPanelListItem>
+            <div className="block-information-content-container">
+                <div className="block-information-content">
+                    <List>
+                        <BlockInformationPanelListItem label="Block Hash" tooltip={tooltip}>
+                            <BlockInformationPanelHash hash={blockInformation.block.blockHash}/>
+                        </BlockInformationPanelListItem>
 
-                    <Divider className="block-information-divider"/>
+                        <Divider className="block-information-divider"/>
 
-                    <BlockInformationPanelListItem label="Block Color" tooltip={tooltip}>
-                        <Typography className={`block-color ${blockColorClass}`} variant="h6">
-                            {blockColorText}
-                        </Typography>
-                    </BlockInformationPanelListItem>
+                        <BlockInformationPanelListItem label="Block Color" tooltip={tooltip}>
+                            <Typography className={`block-color ${blockColorClass}`} variant="h6">
+                                {blockColorText}
+                            </Typography>
+                        </BlockInformationPanelListItem>
 
-                    <Divider className="block-information-divider"/>
+                        <Divider className="block-information-divider"/>
 
-                    <BlockInformationPanelListItem label="Is Block In VSPC" tooltip={tooltip}>
-                        <Typography className="is-block-in-virtual-selected-parent-chain" variant="h6">
-                            {blockInformation.block.isInVirtualSelectedParentChain ? "Yes" : "No"}
-                        </Typography>
-                    </BlockInformationPanelListItem>
+                        <BlockInformationPanelListItem label="Is Block In VSPC" tooltip={tooltip}>
+                            <Typography className="is-block-in-virtual-selected-parent-chain" variant="h6">
+                                {blockInformation.block.isInVirtualSelectedParentChain ? "Yes" : "No"}
+                            </Typography>
+                        </BlockInformationPanelListItem>
 
-                    {!blockInformation.isInformationComplete
-                        ? undefined
-                        : <div>
-                            <Divider className="block-information-divider"/>
+                        {!blockInformation.isInformationComplete
+                            ? undefined
+                            : <div>
+                                <Divider className="block-information-divider"/>
 
-                            <BlockInformationPanelListItem label="Block Parents" tooltip={tooltip}>
-                                {parentElements.length === 0
-                                    ?
-                                    <Typography className="block-information-panel-hash" variant="h6">None</Typography>
-                                    : parentElements
-                                }
-                            </BlockInformationPanelListItem>
+                                <BlockInformationPanelListItem label="Block Parents" tooltip={tooltip}>
+                                    {parentElements.length === 0
+                                        ?
+                                        <Typography className="block-information-panel-hash"
+                                                    variant="h6">None</Typography>
+                                        : parentElements
+                                    }
+                                </BlockInformationPanelListItem>
 
-                            <Divider className="block-information-divider"/>
+                                <Divider className="block-information-divider"/>
 
-                            <BlockInformationPanelListItem label="Block Merge Set" tooltip={tooltip}>
-                                {mergeSetHashElements.length === 0
-                                    ?
-                                    <Typography className="block-information-panel-hash" variant="h6">None</Typography>
-                                    : mergeSetHashElements
-                                }
-                            </BlockInformationPanelListItem>
-                        </div>
-                    }
-                </List>
+                                <BlockInformationPanelListItem label="Block Merge Set" tooltip={tooltip}>
+                                    {mergeSetHashElements.length === 0
+                                        ?
+                                        <Typography className="block-information-panel-hash"
+                                                    variant="h6">None</Typography>
+                                        : mergeSetHashElements
+                                    }
+                                </BlockInformationPanelListItem>
+                            </div>
+                        }
+                    </List>
+                </div>
             </div>
             <div className="katnip-link-text">
                 See more details on <a href={katnipAddressForBlock} target="_blank">Katnip Block Explorer</a>
