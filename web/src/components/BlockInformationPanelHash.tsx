08e5ac7a0fdf2eebac047391aee5293963aaaf5d
stasatdaglabs <stas@daglabs.com>
2021-05-30 16:51:09 +0300

    Style BlockInformationPanelHash tooltips.

--- web/src/components/BlockInformationPanelHash.tsx
+++ web/src/components/BlockInformationPanelHash.tsx
@@ -1,25 +1,18 @@
 import './BlockInformationPanelHash.css'
-import {makeStyles, Tooltip, Typography} from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-    noMaxWidth: {
-        maxWidth: 'none',
-    },
-}));
+import {Typography} from "@material-ui/core";
+import BlockInformationPanelTooltip from "./BlockInformationPanelTooltip";
 
 const BlockInformationPanelHash = ({className, hash}: { className?: string | undefined, hash: string }) => {
     const hashStart = hash.substring(0, 16);
     const hashEnd = hash.substring(hash.length - 8);
 
-    const classes = useStyles();
-
-    return <Tooltip title={hash} arrow interactive classes={{tooltip: classes.noMaxWidth}}>
+    return <BlockInformationPanelTooltip title={<div className="block-information-panel-tooltip">{hash}</div>}>
         <Typography className={`block-information-panel-hash ${className}`} variant="h6">
             <div className="hash-start">{hashStart}</div>
             <div className="hash-ellipsis">…</div>
             <div className="hash-end">{hashEnd}</div>
         </Typography>
-    </Tooltip>;
+    </BlockInformationPanelTooltip>;
 };
 
 export default BlockInformationPanelHash;
---
+++ web/src/components/BlockInformationPanelTooltip.tsx
@@ -0,0 +1,29 @@
+import {ReactChild, ReactElement, ReactFragment, ReactPortal} from "react";
+import {makeStyles, Tooltip} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+    tooltip: {
+        maxWidth: "none",
+        backgroundColor: "#175676",
+        border: "1px solid #ffffff",
+        fontSize: "0.9rem",
+    },
+    arrow: {
+        "&::before": {
+            backgroundColor: "#175676",
+            border: "1px solid #ffffff",
+        },
+    }
+}));
+
+const BlockInformationPanelTooltip = ({children, title}:
+                                          { children: ReactElement, title: boolean | ReactChild | ReactFragment | ReactPortal }) => {
+
+    const classes = useStyles();
+
+    return <Tooltip title={title} arrow interactive classes={{tooltip: classes.tooltip, arrow: classes.arrow}}>
+        {children}
+    </Tooltip>
+};
+
+export default BlockInformationPanelTooltip;
