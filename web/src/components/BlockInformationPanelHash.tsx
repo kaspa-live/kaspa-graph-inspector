7fc9b259c5fbb83184c362722e9d22680fcf3b3e
stasatdaglabs <stas@daglabs.com>
2021-05-30 15:23:11 +0300

    Make hash tooltips in the block information panel not word-wrap.

--- web/src/components/BlockInformationPanelHash.tsx
+++ web/src/components/BlockInformationPanelHash.tsx
@@ -1,11 +1,19 @@
 import './BlockInformationPanelHash.css'
-import {Tooltip, Typography} from "@material-ui/core";
+import {makeStyles, Tooltip, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    noMaxWidth: {
+        maxWidth: 'none',
+    },
+}));
 
 const BlockInformationPanelHash = ({className, hash}: { className?: string | undefined, hash: string }) => {
     const hashStart = hash.substring(0, 16);
     const hashEnd = hash.substring(hash.length - 8);
 
-    return <Tooltip title={hash} arrow interactive>
+    const classes = useStyles();
+
+    return <Tooltip title={hash} arrow interactive classes={{tooltip: classes.noMaxWidth}}>
         <Typography className={`block-information-panel-hash ${className}`} variant="h6">
             <div className="hash-start">{hashStart}</div>
             <div className="hash-ellipsis">…</div>
