95b0e2c48855b472cd69236facae76afacbd56e3
tiram88 <georges.kuenzli@cepec.com>
2022-05-04 08:32:42 +0200

    Configure @mui/material/styles set to @emotion

---
+++ web/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    [
+      "@emotion",
+      {
+        importMap: {
+          "@mui/system": {
+            styled: {
+              canonicalImport: ["@emotion/styled", "default"],
+              styledBaseImport: ["@mui/system", "styled"]
+            }
+          },
+          "@mui/material/styles": {
+            styled: {
+              canonicalImport: ["@emotion/styled", "default"],
+              styledBaseImport: ["@mui/material/styles", "styled"]
+            }
+          }
+        }
+      }
+    ]
+  ]
+};
