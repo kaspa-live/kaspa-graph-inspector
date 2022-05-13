f7005c1dcef39ffccc14268a3d18e1412d74aa5a
tiram88 <georges.kuenzli@cepec.com>
2022-05-14 00:14:11 +0200

    Refine block information panel transition

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { createTheme, ThemeProvider, StyledEngineProvider, Box } from '@mui/material';
+import { createTheme, ThemeProvider, StyledEngineProvider, Box, Slide } from '@mui/material';
+import React from 'react';
 import Dag from "./dag/Dag";
 import {useState} from "react";
 import BlockInformationPanel from "./components/panel/bloc-information/BlockInformationPanel";
@@ -8,6 +9,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import {BlockInformation} from "./model/BlockInformation";
 
 const App = () => {
+    const containerRef = React.useRef(null);
     const [blockInformationState, setBlockInformationState] = useState<BlockInformation | null>(null);
     const [wasBlockSetState, setWasBlockSetState] = useState(false);
     const [wasBlockInformationPanelCloseRequested, setBlockInformationPanelCloseRequested] = useState(false);
@@ -33,32 +35,58 @@ const App = () => {
         setWasBlockSetState(wasBlockSetState || blockInformation !== null);
     });
 
+    const transitionDuration = {
+        enter: theme.transitions.duration.enteringScreen * 2,
+        exit: theme.transitions.duration.leavingScreen * 3,
+    };
+
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={theme}>
-                <Box sx={{
+                <Box
+                    sx={{
                         minHeight: '100vh',
                         minWidth: '100vw',
                         display: 'flex',
                         flexDirection: 'column',
                     }}
+                    ref={containerRef}
                 >
                     <Canvas dag={dag}/>
                     <Sidebar dag={dag}/>
 
                     {!wasBlockSetState ? undefined :
-                        <div
-                            className={`block-information-container ${isBlockInformationPanelOpenState ? "block-information-open" : "block-information-closed"}`}>
-                            <BlockInformationPanel blockInformation={blockInformationState}
-                                                   onClose={() => {
-                                                       setBlockInformationPanelCloseRequested(true);
-                                                       setBlockInformationPanelOpenState(false);
-                                                   }}
-                                                   onClickHash={(hash: string) => {
-                                                        dag.setStateTrackTargetHash(hash);
-                                                   }}/>
-                        </div>
+
+                        <Slide
+                            direction="right"
+                            in={isBlockInformationPanelOpenState}
+                            container={containerRef.current}
+                            timeout={transitionDuration}
+                            style={{
+                                transitionDelay: `${isBlockInformationPanelOpenState ? 0 : theme.transitions.duration.leavingScreen}ms`,
+                            }}            
+                        >
+                            <Box
+                                sx={{
+                                    position: "absolute",
+                                    height: "100vh",
+                                    minWidth: "320px",
+                                }}
+                            >
+                                <BlockInformationPanel
+                                    blockInformation={blockInformationState}
+                                    onClose={() => {
+                                        setBlockInformationPanelCloseRequested(true);
+                                        setBlockInformationPanelOpenState(false);
+                                    }}
+                                    onClickHash={(hash: string) => {
+                                            dag.setStateTrackTargetHash(hash);
+                                    }}
+                                />
+                            </Box>
+                        </Slide>
                     }
+
                 </Box>
             </ThemeProvider>
         </StyledEngineProvider>
